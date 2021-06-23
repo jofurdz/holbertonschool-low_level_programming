e36636d77c7acf13d3b80a601736a13f08cca804
jofurdz <jofurdz@gmail.com>
2021-06-23 12:46:07 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+* main - printing specific line ending with a new one
+*Return: 0
+*/
+int main(void)
+{printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0); }
