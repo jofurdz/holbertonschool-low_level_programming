2d9ab6d42245d4a98908b871321e2da3213ebe9a
jofurdz <jofurdz@gmail.com>
2021-06-23 12:06:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+* main - printing specific line and ending with a new one
+* Return: 0
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0); }
