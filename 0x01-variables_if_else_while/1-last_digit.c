d2aa97dbfa8bef3a4e45f692414260bcb2d3f670
jofurdz <jofurdz@gmail.com>
2021-06-25 13:02:03 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is 0/n", n, p);
 }
 else if (p < 6)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, p);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, p);
 }
 return (0);
 }
