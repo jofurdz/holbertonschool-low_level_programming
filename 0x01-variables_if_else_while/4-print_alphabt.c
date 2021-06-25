31c00af22162adf4e58e1c204b79353e35b64c7b
jofurdz <jofurdz@gmail.com>
2021-06-25 15:54:14 -0700

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - printing the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+  if (x != e || q); 
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - printing the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
