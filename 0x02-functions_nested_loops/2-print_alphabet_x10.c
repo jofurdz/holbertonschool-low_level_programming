677e64a480c263c3c061460fec3a8d906dbeb0de
jofurdz <jofurdz@gmail.com>
2021-06-28 11:36:56 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 void print_alphabet_x10(void)
 {
 int x = 0;
-char p;
-while(x++ <= 9)
+char ch;
+while (x++ <= 9)
 {
-for (p = 'a'; p <= 'z'; p++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-_putchar(x);
+_putchar(ch);
 }
 _putchar('\n');
 }
