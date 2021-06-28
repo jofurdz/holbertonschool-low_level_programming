e29739b6be95f840a41958494a12a6f221f169b4
jofurdz <jofurdz@gmail.com>
2021-06-28 12:00:42 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 /**
-* main - checking for lowercase character
+* _islower - checking for lowercase character
+* @c: returning 1 if lower
+*
 *Return: 0
 */
 int _islower(int c)
