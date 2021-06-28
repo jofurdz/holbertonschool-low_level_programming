2e2c932164c82d49a8b99b5caef2158c2fe4b87a
jofurdz <jofurdz@gmail.com>
2021-06-28 12:50:59 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _abs - computing absolute value of an integer
+* @x: the character to print
+*Return: 0
+*/
+int _abs(int x)
+{
+if (x >= 0)
+{
+return (x);
+}
+else
+return (-x);
+}
