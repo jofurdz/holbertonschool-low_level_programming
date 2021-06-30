23edaa354a3c8cc3fe3ef51a04a56e6ea4cb33bf
jofurdz <jofurdz@gmail.com>
2021-06-29 17:02:15 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,4 +5,17 @@
 *Return: 0
 */
 int main(void)
-  
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+putchar (x + '0');
+if (x != 9)
+{
+putchar (',');
+putchar (' ');
+}
+}
+putchar ('\n');
+return (0);
+}
