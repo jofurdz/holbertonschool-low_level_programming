a4008c8fb40a0e4e63efeebd353f1ba29811c265
jofurdz <jofurdz@gmail.com>
2021-07-01 08:14:02 -0700

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - assigning a random number to the variable n each time it is executed
+*Return: 0
+*/
+int main(void)
+{
+int n;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
