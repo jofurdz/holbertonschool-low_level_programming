74f01b848f138b5983e0cf133c6c3ebc33a91683
jofurdz <jofurdz@gmail.com>
2021-07-01 18:34:47 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,8 @@ for (y = 0; y <= 9; y++)
 for (x = 0; x <= 14; x++)
 {
 if (x > 9)
-_putchar ((x/10) + '0'); 
-_putchar ((x%10) + '0');
+_putchar ((x / 10) + '0');
+_putchar ((x % 10) + '0');
 }
 _putchar ('\n');
 }
