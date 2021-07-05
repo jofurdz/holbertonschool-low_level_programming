cd36136b868a5d052a8593b452e7012adc8ab6e6
jofurdz <jofurdz@gmail.com>
2021-07-04 23:20:58 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,23 @@
 #include "holberton.h"
 /**
+* print_rev - prints a string in reverse
 *
-*
-*
+* @s: string to be printed
 */
 void print_rev(char *s)
 {
+int length = 0;
+while (*s)
+{
+s++;
+length++;
+}
+while (length >= 0)
+{
+length--;
+_putchar (*s);
+s--;
+}
+_putchar ('\n');
 
 }
