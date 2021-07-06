a935fbeb5b6959f5a276b01e2183a893e359e430
jofurdz <jofurdz@gmail.com>
2021-07-06 07:51:26 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include "2-strlen.c"
 /**
 * puts_half - printing half of a string
 *
@@ -6,11 +7,19 @@
 */
 void puts_half(char *str)
 {
-int length = 0;
-if (length % 2 == 0);
-while (*str)
+int length = 0, i, mid;
+length = _strlen(str);
+if (length % 2 == 0)
 {
-length++;
-_putchar (*str);
+mid = length / 2;
 }
+else
+{
+mid = (length + 1) / 2;
+}
+for (i = mid; i < length; i++)
+{
+_putchar (str[i]);
+}
+_putchar ('\n');
 }
