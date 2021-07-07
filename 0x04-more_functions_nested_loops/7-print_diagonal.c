17ff795251b9fa9b631244a393aae0501349913b
jofurdz <jofurdz@gmail.com>
2021-07-07 11:56:40 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,4 +4,22 @@
 * 
 *Return: 0
 */
-void print_diagonal(int n);
+void print_diagonal(int target)
+{
+  int lines;
+  int spaces;
+  if (target > 0)
+    {
+  for (lines = 0; lines < target; lines++)
+    {
+		for (spaces = 0; spaces < lines; spaces++)
+      {
+	_putchar (' ');
+    }
+		_putchar ('\\');
+      _putchar ('\n');
+    }
+    }
+  else
+  _putchar ('\n');
+	}
