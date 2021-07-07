d6307ceaf0bf52ca86211d2e941d0073c0e33f74
jofurdz <jofurdz@gmail.com>
2021-07-06 18:34:41 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,9 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; i < n - 1; i++)
 {
-if (i == n - 1)
-printf("%d\n", a[i]);
-else
 printf("%d, ", a[i]);
 }
+printf("%d\n", a[i]);
 }
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* *_strcpy - copies the string, including the null byte
+*
+* @dest: string to be pointed to
+* @src: string to be pointed to
+*
+*Return: 0
+*/
+char *_strcpy(char *dest, char *src)
+{
