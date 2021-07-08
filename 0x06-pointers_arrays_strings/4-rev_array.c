f7c9c616d837db4517b1ca3ac78bd8300dcd71d0
jofurdz <jofurdz@gmail.com>
2021-07-08 15:40:23 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 /**
 * reverse_array - reverses array of integers
-*
-* @a - int to be printed
-* @n - int to be printed
+* @a: int to be printed
+* @n: int to be printed
 */
 void reverse_array(int *a, int n)
 {
-	
+	int low;
+	int high;
+for (low = 0, high = n - 1; low < high; low++, high--)
+{
+int temp = a[low];
+a[low] = a[high];
+a[high] = temp;
+}
 }
