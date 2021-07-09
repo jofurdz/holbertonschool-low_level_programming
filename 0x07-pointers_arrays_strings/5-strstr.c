20052660ef0709cd12f679617a628a17decf6d3e
jofurdz <jofurdz@gmail.com>
2021-07-09 14:45:00 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,14 +2,15 @@
 #include <string.h>
 /**
  * _strstr - function that locates a substring
- * @haystack
- *
+ * @haystack: char to br printed
+ * @needle: char to be printed
+ *Return: 0
  */
 char *_strstr(char *haystack, char *needle)
 {
 size_t n = strlen(needle);
-    while(*haystack)
-        if(!memcmp(haystack++,needle,n))
-            return (char *) (haystack-1);
-    return 0;
+while (*haystack)
+if (!memcmp(haystack++, needle, n))
+return ((char *) (haystack - 1));
+return (0);
 }
