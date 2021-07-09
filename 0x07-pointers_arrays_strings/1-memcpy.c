47b959e88752e111541d14ec5c5ece4bc38056e1
jofurdz <jofurdz@gmail.com>
2021-07-09 10:44:56 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * _memcpy - copies memory area
+ * @dest: char to be printed
+ * @src: char to be printed
+ * @n: int to be printed
+ *Return: 0
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
-	
 	char *csrc = (char *)src;
 	char *cdest = (char *)dest;
 	char *temp = new char[n];
