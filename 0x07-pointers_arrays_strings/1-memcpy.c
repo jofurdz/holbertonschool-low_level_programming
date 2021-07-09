b995c4294dddeb47dbb37fd86675ed4740e49938
jofurdz <jofurdz@gmail.com>
2021-07-09 15:33:09 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,16 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
-	char *temp = new char[n];
-	for (int i=0; i<n; i++)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		temp[i] = csrc[i];
+		dest[i] = src[i];
 	}
-	for (int i=0; i<n; i++)
-	{
-		cdest[i] = temp[i];
-	}
+	return (dest);
 }
