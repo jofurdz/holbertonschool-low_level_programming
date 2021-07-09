fd87d9c975dd60954191fcbb798c223eb08fa484
jofurdz <jofurdz@gmail.com>
2021-07-09 15:08:15 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: char to be printed
+ *Return: 0
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 123, 125};
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				s[i] -= 32;
+			}
+			else
+			{
+				for (j = 0; j < 14; j++)
+				{
+					if (s[i - 1] == sep[j])
+						s[i] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
