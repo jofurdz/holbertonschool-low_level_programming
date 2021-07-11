dfef3451d76cd00fca050ab158619234b9b5cd81
jofurdz <jofurdz@gmail.com>
2021-07-11 07:26:16 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <string.h>
 /**
  *
  *
@@ -6,5 +7,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	int i = 0;
 
+	if ((*s == '\0') || (*accept == '\0'))
+		return (i);
+	while (*s && strchr(accept, *s++))
+	{
+		i++;
+	}
+	return (i);
 }
