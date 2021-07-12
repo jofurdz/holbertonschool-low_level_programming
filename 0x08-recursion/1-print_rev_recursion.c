85d984d09d7828691075944fe9c458229c28c8a1
jofurdz <jofurdz@gmail.com>
2021-07-12 08:43:50 -0700

    task 3

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * print_rev_recursion - prints a string in reverse
+ *
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * factorial - returns the factorial of a number
+ *
+ * @n: int to be printed
+ *Return: 0
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	else
+	return (n * factorial(n - 1));
+}
