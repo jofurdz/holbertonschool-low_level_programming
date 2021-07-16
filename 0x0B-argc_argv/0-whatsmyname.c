5fa7794b13cc0749047f099a16560dc7521ef36d
jofurdz <jofurdz@gmail.com>
2021-07-16 09:56:58 -0500

    task 0

---
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints name followed by new lin
+ * 
+ * @argc: unused argument
+ * @argv: name of our program
+ * 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
