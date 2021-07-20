af1cbcbb2ab568d9996096315f4da98c44a0fb22
jofurdz <jofurdz@gmail.com>
2021-07-20 13:57:59 -0500

    task 1

--- 0x0C-malloc_free/1-strdup.c
+++ 0x0C-malloc_free/1-strdup.c
@@ -13,9 +13,17 @@ char *_strdup(char *str)
 	char *p;
 	int len = 0;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	while (str[len])
 		len++;
 	x = malloc(len + 1);
+	if (x == NULL)
+	{
+		return (NULL);
+	}
 	p = x;
 	while (*str)
 		*p++ = *str++;
