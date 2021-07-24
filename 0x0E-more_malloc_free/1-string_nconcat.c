ff458febbfff8ab33d957969e2b94fea4e0c50cb
jofurdz <jofurdz@gmail.com>
2021-07-23 19:11:26 -0500

    task 1

--- 0x0E-more_malloc_free/1-string_nconcat.c
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		length2++;
 	}
-	blah = malloc(length1 + length2 + 1);
+	blah = malloc((length1 + n + 1) * sizeof(char));
 
 	if (blah == NULL)
 	{
