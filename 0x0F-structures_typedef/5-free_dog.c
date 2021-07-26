d5d8bebe0f098c924426ea6e63b9a77b1d3c2609
jofurdz <jofurdz@gmail.com>
2021-07-26 13:59:37 -0500

    task 5

--- 0x0F-structures_typedef/5-free_dog.c
+++ 0x0F-structures_typedef/5-free_dog.c
@@ -1,11 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 /**
- * 
- * 
- * 
- * 
+ * free_dog - frees dogs
+ *
+ * @d: name of our string
+ *
+ *Return: 0
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
