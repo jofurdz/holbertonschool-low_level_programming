246bba31601e88064d82816af787f447041a42fe
jofurdz <jofurdz@gmail.com>
2021-07-29 12:29:46 -0500

    task 1

--- 0x11-variadic_functions/1-print_numbers.c
+++ 0x11-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(blah, int));
 
-		if (separator != NULL && x != n)
+		if (separator != NULL && x != (n - 1))
 		{
 			printf("%s", separator);
 		}
