2f295bd7b7e089c2b1dc206709e7e3a3d889022e
jofurdz <jofurdz@gmail.com>
2021-08-12 10:02:00 -0500

    task 1

---
+++ 0x15-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - prints a binary number
+ *
+ * @n: number to be printed
+ *
+ *Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
+}
