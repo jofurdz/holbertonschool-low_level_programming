3c90f284f3407870d16443db2ccd9fddeaeaf178
jofurdz <jofurdz@gmail.com>
2021-08-12 12:08:39 -0500

    task 5

---
+++ 0x15-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * clear_bit - sets value of bit to 0 at given index
+ *
+ * @n: bit to be printed
+ * @index: index to be cleared
+ *
+ *Return: -1 if it fails and 1 on success
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n = (1 << index);
+	return (1);
+}
---
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * countSetBits - counts bits to be set
+ *
+ * @n: bit to be reset
+ *
+ *Return: count
+ */
+
+int countSetBits(int n)
+{
+	int count = 0;
+
+	while (n > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+	return (count);
+}
+
+/**
+ * flip_bits - flips bits
+ *
+ * @n: bits to be reset
+ * @m: index to be flipped
+ *
+ *Return: -1 on failure and 1 on success
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (countSetBits(n ^ m));
+}
