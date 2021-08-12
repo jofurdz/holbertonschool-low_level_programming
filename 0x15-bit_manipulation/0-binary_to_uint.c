b35866f7abdd96a726d55a528f12ec4172e20455
jofurdz <jofurdz@gmail.com>
2021-08-12 13:00:49 -0500

    moved if check out of for loop

--- 0x15-bit_manipulation/0-binary_to_uint.c
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 	for (val = 0; *b; b++)
 	{
-		if (b == NULL)
-		{
-			return (0);
-		}
 		if (*b != '0' && *b != '1')
 		{
 			return (0);
