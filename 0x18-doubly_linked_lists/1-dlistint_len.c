4e3f796e2e9edbbefc71b01e6ecdf655270eed9a
jofurdz <jofurdz@gmail.com>
2021-09-07 13:00:49 -0500

    task 0-2

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
