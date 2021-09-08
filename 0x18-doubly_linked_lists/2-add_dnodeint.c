5c81f0998103d1455f532e484944cc1302980665
jofurdz <jofurdz@gmail.com>
2021-09-08 10:50:05 -0500

    fixed task 2

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
-	new_node->next = (*head);
-	(*head) = new_node;
-
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		(*head) = new_node;
+	}
+	else
+	{
+		(*head)->prev = new_node;
+		new_node->next = (*head);
+		(*head) = new_node;
+	}
 	return (new_node);
 }
